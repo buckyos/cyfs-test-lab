f7de4d42910d64f786a5d53616ffaaf1242fd747
lizhihong <lizhihong@buckyos.com>
2022-08-08 20:27:03 +0800

    Upload testcase

--- src/node-tester-app/tasks/CYFS_debuger/onload.ts
+++ src/node-tester-app/tasks/CYFS_debuger/onload.ts
@@ -6,14 +6,14 @@ var date = require("silly-datetime");
 
 
 export async function TaskMain(_interface: TaskClientInterface) {
-    // 测试用例demo说明: NFT2 机器使用CYFS协议栈ts_client 进行put_object 并且 get_object 检查数据操作，共执行100次，同时并发执行任务数10；
+    // 测试用例demo说明: Zone2_Device1 机器使用CYFS协议栈ts_client 进行put_object 并且 get_object 检查数据操作，共执行100次，同时并发执行任务数10；
     
     // 测试节点
     let agentList:Array<Agent> = [
         {
-            name : "NFT2", //名称标签
+            name : "Zone2_Device1", //名称标签
             cyfs_clients : [{
-                name:"NFT2_ts_client", //   模拟协议栈 ${Agent.name}_0 、${Agent.name}_1 这样编号
+                name:"Zone2_Device1_ts_client", //   模拟协议栈 ${Agent.name}_0 、${Agent.name}_1 这样编号
                 type:"runtime", //协议栈client 连接类型 runtime 、ood 、port
                 SDK_type:"typescript",
             }],
@@ -27,20 +27,20 @@ export async function TaskMain(_interface: TaskClientInterface) {
     let taskList:Array<Task> = []
     for(let i =0;i<100;i++){
         taskList.push({
-            LN : {name:"NFT2_ts_client"}, 
-            RN : {name:"NFT2_ts_client"},  // remote
+            LN : {name:"Zone2_Device1_ts_client"}, 
+            RN : {name:"Zone2_Device1_ts_client"},  // remote
             clients : [],
             timeout : 100*1000, //超时时间
             action:[{
                 type : "put_object_check",
-                source : "NFT2_ts_client",
-                target : "NFT2_ts_client",
+                source : "Zone2_Device1_ts_client",
+                target : "Zone2_Device1_ts_client",
                 timeout : 100*1000,
                 input_data : {
                     obj_type : CustumObjectType.MyText,
                     common : {
                         level: "router",
-                        target: "NFT2_ts_client",
+                        target: "Zone2_Device1_ts_client",
                         flags : 0
         
                     }
