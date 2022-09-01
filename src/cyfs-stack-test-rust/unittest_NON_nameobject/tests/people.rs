8701a089460f3d0de4dcd3ab4d874e9496c3f2d3
zwong91 <yzbingchuan@gmail.com>
2022-09-01 18:47:29 +0800

    peopele encode/decode

--- src/cyfs-stack-test-rust/unittest_NON_nameobject/tests/people.rs
+++ src/cyfs-stack-test-rust/unittest_NON_nameobject/tests/people.rs
@@ -1,12 +1,24 @@
 use async_std::{future, io::prelude::*, task};
 use cyfs_base::*;
+use cyfs_util::*;
 use std::path::Path;
-use std::fs::{create_dir};
 
 // cargo test -- --nocapture
 #[async_std::test]
 async fn people() {
-    let private_key = PrivateKey::generate_rsa(1024).unwrap();
+    let root = ::cyfs_util::get_cyfs_root_path().join("etc").join("desc");
+    // 加载密钥
+    let sec_file_name = format!("people.sec");
+    let sec_file = root.join(sec_file_name);
+    println!("{}", sec_file.display());
+    if !sec_file.is_file() {
+        let private_key = PrivateKey::generate_rsa(1024).unwrap();
+        let _ = private_key.encode_to_file(sec_file.as_path(), false);
+    }
+
+    let mut buf: Vec<u8> = Vec::new();
+    let ret = PrivateKey::decode_from_file(sec_file.as_path(), &mut buf);
+    let (private_key, _) = ret.unwrap();
 
     let pubic_key = private_key.public();
 
@@ -22,9 +34,8 @@ async fn people() {
 
     p.set_name("people".to_owned());
 
-    let path = Path::new("c:\\test\\people.obj");
-    create_dir("c:\\test").unwrap();
-    if path.parent().unwrap().exists() {
+    let path = Path::new("../../people.obj");
+    if !path.exists() {
         let _ = p.encode_to_file(path, false);
     }
 
@@ -36,4 +47,9 @@ async fn people() {
 
     assert_eq!(p.desc().people_id(), pp.desc().people_id());
     assert_eq!(p.name(), pp.name());
+    let mut buf = vec![];
+    
+    let (p1, _) = People::decode_from_file(&path, &mut buf).unwrap();
+    assert_eq!(p.desc().people_id(), p1.desc().people_id());
+    assert_eq!(p.name(), p1.name());
 }
