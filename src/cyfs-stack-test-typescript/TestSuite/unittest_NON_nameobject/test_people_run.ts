9cfc2329f405efae229cc297868a6260d92945c7
lixiang <lixiang@buckyos.com>
2022-09-01 20:09:24 +0800

    update people

--- src/cyfs-stack-test-typescript/TestSuite/unittest_NON_nameobject/test_people_run.ts
+++ src/cyfs-stack-test-typescript/TestSuite/unittest_NON_nameobject/test_people_run.ts
@@ -4,6 +4,7 @@ import * as fs from 'fs';
 import { descpath, run, decoder, DeleteDescFile } from './index';
 import { copySync } from "fs-extra";
 var assert = require("assert");
+import {RandomGenerator} from "../../common";
 
 
 //People对象测试
@@ -15,8 +16,6 @@ describe("测试People对象编解码", function () {
     let deviceidstr2 = '5aUiNsqWyZFmJNndx5dTDaTASNJPmj8k8npCdq9z9D16';
     let fileidstr = '7Tk94YfZjQQETp7wnMZPg9CiqZWNDwSTAxnXfCAG62Vu';
     let name = "TEST123456!@#$%^";
-    let name_e = "";
-    let name2 = "修改name！@#￥%……&*（";
 
     let owner = cyfs.ObjectId.from_base_58(objectidstr).unwrap();
     let deviceid = cyfs.DeviceId.from_base_58(deviceidstr).unwrap();
@@ -35,6 +34,7 @@ describe("测试People对象编解码", function () {
     let fileidstr1 = '7Tk94Yfh3i5r5fFtvPMZqreFA9tcYfrhMext8dVDU5Zi'
     let fileid1 = cyfs.FileId.from_base_58(fileidstr1).unwrap()
 
+    let PeopleId: cyfs.ObjectId
     //定义文件路径：
     let filepath = descpath('People');
     let filepath1 = descpath('People01');
@@ -59,7 +59,7 @@ describe("测试People对象编解码", function () {
             )
             let u8people = People.to_vec().unwrap()
             fs.writeFileSync(filepath, u8people);
-            let PeopleId = People.desc().calculate_id();
+            PeopleId = People.desc().calculate_id();
 
             let [target, buffer] = new cyfs.PeopleDecoder().raw_decode(u8people).unwrap();
 
@@ -78,9 +78,8 @@ describe("测试People对象编解码", function () {
             for (let i in ood_list_deco) {
                 assert(ood_list_deco[i].equals(ood_list1[i]), 'ood_list属性不相等')
             }
-
-
         });
+
         it("Ts编码：有效传入多个DeviceId组成的ood_list参数", async function () {
             let People = cyfs.People.create(
                 cyfs.Some(owner),
@@ -112,17 +111,19 @@ describe("测试People对象编解码", function () {
                 icon,
             )
             let u8people = People.to_vec().unwrap()
+            let enood_list = People.body_expect().content().ood_list
             fs.writeFileSync(filepath2, u8people);
             let [target, buffer] = new cyfs.PeopleDecoder().raw_decode(u8people).unwrap();
 
             //获取属性
-            let ood_list_deco = target.body_expect().content().ood_list
-
+            let deood_list = target.body_expect().content().ood_list
             //属性校验
-           
-            assert.equal(ood_list_deco,ood_list, 'ood_list属性不相等')
-        
-            
+
+            console.log("en----------"+typeof(enood_list))
+            console.log("de----------"+typeof(deood_list))
+            assert.equals(deood_list, deviceid3, 'ood_list属性不相等')
+
+
         });
         it("Ts编码：有效传入空值的owner,name参数", async function () {
             let People = cyfs.People.create(
@@ -130,7 +131,7 @@ describe("测试People对象编解码", function () {
                 ood_list1,
                 secret.public(),
                 cyfs.None,
-                name_e,
+                "",
                 icon,
             )
             let u8people = People.to_vec().unwrap()
@@ -157,7 +158,7 @@ describe("测试People对象编解码", function () {
 
             //属性校验
             assert.equal(undefined, target.icon());
-            assert.equal("", target.name());
+            assert.equal(undefined, target.name());
         });
         it("Ts编码：有效调用set_name()方法，修改name属性", async function () {
             let People = cyfs.People.create(
@@ -168,6 +169,7 @@ describe("测试People对象编解码", function () {
                 name,
                 icon,
             )
+            let name2 = "修改name！@#￥%……&*（"
             People.set_name(name2)
             let u8people = People.to_vec().unwrap()
             fs.writeFileSync(filepath5, u8people);
@@ -200,7 +202,7 @@ describe("测试People对象编解码", function () {
 
             //属性校验
             assert(icon_deco?.equals(fileid1), 'icon属性不相等');
-    
+
         });
         it("Ts编码：设置name,icon参数为undefined", async function () {
             let People = cyfs.People.create(
@@ -214,17 +216,20 @@ describe("测试People对象编解码", function () {
             let u8people = People.to_vec().unwrap()
             fs.writeFileSync(filepath7, u8people);
             let [target, buffer] = new cyfs.PeopleDecoder().raw_decode(u8people).unwrap();
-             //属性校验
-             assert.equal(undefined, target.icon());
-             assert.equal(undefined, target.name());
+            //属性校验
+            assert.equal(undefined, target.icon());
+            assert.equal(undefined, target.name());
         });
         it("Ts编码：设置name足够大2^16 Byte", async function () {
+
+            let strRandom = RandomGenerator.string(10000,10000,10000)
+
             let People = cyfs.People.create(
                 cyfs.Some(owner),
                 ood_list1,
                 secret.public(),
                 cyfs.Some(area),
-                name,
+                strRandom,
                 icon,
             )
             People.set_icon(fileid1)
@@ -234,94 +239,118 @@ describe("测试People对象编解码", function () {
         });
     });
 
-    // describe("Rust编解码desc-tool.exe", function () {
-    //     it("Rust工具解码(Ts编码对象)：有效对Ts编码对象进行Rust工具解码", async function () {
-    //         let ffsClientPath = __dirname + '/test-tool/tool/desc-tool.exe';
-    //         let args = ' show -a ' + filepath1;
-    //         let runInfo = await run(ffsClientPath, args, false, (data: string) => {
-    //             let arr1 = data.split(/: |\n/);
-    //             for (let i in arr1) {
-    //                 let value = arr1[i];
-    //                 let index: any = Number(i) + 1;
-    //                 //校验PeopleId属性
-    //                 if (value == 'objectid') {
-    //                     let objectid_rust = arr1[index]
-    //                     assert.equal(PeopleId.to_base_58(), objectid_rust)
-    //                 };
-    //                 //校验owner属性
-    //                 if (value == 'owner') {
-    //                     let owner_rust = arr1[index]
-    //                     assert.equal(objectidstr, owner_rust)
-    //                 }
-    //                 //校验ood_list属性
-    //                 if (value == 'ood_list') {
-    //                     let ood_list_rust = arr1[index]
-    //                     let ood_rust = ood_list_rust.slice(1, ood_list_rust.length - 3)
-    //                     assert.equal(deviceidstr, ood_rust)
-    //                 }
-    //                 //校验name属性
-    //                 if (value == 'name') {
-    //                     let name_rust = arr1[index]
-    //                     assert.equal(name, name_rust)
-    //                 }
-    //                 //校验icon属性
-    //                 if (value == 'icon') {
-    //                     let icon_rust = arr1[index]
-    //                     assert.equal(fileidstr, icon_rust)
-    //                 }
-    //             }
-    //         });
-    //     });
-    //     it("Rust工具编码->Ts解码：有效进行Rust工具编码对象，再进行Ts解码流程", async function () {
-    //         //rust创建对象
-    //         //创建cmd命令：desc-tool.exe create people --area 1:2:3:4 --pktype rsa1024 --owner "5r4MYfFdhhaG9ENa8ED1AYRttuGNYDBiaZdpBHGsW7oC"  --oodlist "5aUiNsqh5oSZnwaEb8wj7rwSouqGNuEgjF3pLB1y4pGQ"
-    //         let ffsClientPath = __dirname + '/test-tool/tool/desc-tool.exe';
-    //         let args1 = ' create people --area 1:2:3:4 --pktype rsa1024 --owner "5r4MYfFdhhaG9ENa8ED1AYRttuGNYDBiaZdpBHGsW7oC"  --oodlist "5aUiNsqh5oSZnwaEb8wj7rwSouqGNuEgjF3pLB1y4pGQ" ';
-    //         var rust_desc = ''
-    //         let runInfo1 = await run(ffsClientPath, args1, false, (data: string) => {
-    //             rust_desc = data.slice(data.length - 50, data.length - 6)
-    //         })
+    describe("Rust编解码desc-tool.exe", function () {
+        it("Rust工具解码(Ts编码对象)：有效对Ts编码对象进行Rust工具解码", async function () {
+            let ffsClientPath = __dirname + '/test-tool/tool/desc-tool.exe';
+            let args = ' show -a ' + filepath;
+            let runInfo = await run(ffsClientPath, args, false, (data: string) => {
+                let arr1 = data.split(/: |\n/);
+                for (let i in arr1) {
+                    let value = arr1[i];
+                    let index: any = Number(i) + 1;
+                    //校验PeopleId属性
+                    if (value == 'objectid') {
+                        let objectid_rust = arr1[index]
+                        assert.equal(PeopleId.to_base_58(), objectid_rust)
+                    };
+                    //校验owner属性
+                    if (value == 'owner') {
+                        let owner_rust = arr1[index]
+                        assert.equal(objectidstr, owner_rust)
+                    }
+                    //校验ood_list属性
+                    if (value == 'ood_list') {
+                        let ood_list_rust = arr1[index]
+                        let ood_rust = ood_list_rust.slice(1, ood_list_rust.length - 3)
+                        assert.equal(deviceidstr, ood_rust)
+                    }
+                    //校验name属性
+                    if (value == 'name') {
+                        let name_rust = arr1[index]
+                        assert.equal(name, name_rust)
+                    }
+                    //校验icon属性
+                    if (value == 'icon') {
+                        let icon_rust = arr1[index]
+                        assert.equal(fileidstr, icon_rust)
+                    }
+                }
+            });
+        });
+        it("Rust工具编码->Ts解码：有效进行Rust工具编码对象，再进行Ts解码流程", async function () {
+            //rust创建对象
+            //创建cmd命令：desc-tool.exe create people --area 1:2:3:4 --pktype rsa1024 --owner "5r4MYfFdhhaG9ENa8ED1AYRttuGNYDBiaZdpBHGsW7oC"  --oodlist "5aUiNsqh5oSZnwaEb8wj7rwSouqGNuEgjF3pLB1y4pGQ"
+            let ffsClientPath = __dirname + '/test-tool/tool/desc-tool.exe';
+            let args1 = ' create people --area 1:2:3:4 --pktype rsa1024 --owner "5r4MYfFdhhaG9ENa8ED1AYRttuGNYDBiaZdpBHGsW7oC"  --oodlist "5aUiNsqh5oSZnwaEb8wj7rwSouqGNuEgjF3pLB1y4pGQ" ';
+            var rust_desc = ''
+            let runInfo1 = await run(ffsClientPath, args1, false, (data: string) => {
+                rust_desc = data.slice(data.length - 50, data.length - 6)
+            })
 
-    //         //Ts解码(Ts编码对象)对象,获取属性
-    //         let rustfilepath = __dirname + '/test-tool/tool/' + rust_desc + '.desc'
-    //         let rustfilesecpath = __dirname + '/test-tool/tool/' + rust_desc + '.sec'
-    //         let desc_buffer = decoder(rustfilepath)
-    //         let [target, buffer] = new cyfs.PeopleDecoder().raw_decode(desc_buffer).unwrap();
+            //Ts解码(Ts编码对象)对象,获取属性
+            let rustfilepath = __dirname + '/test-tool/tool/' + rust_desc + '.desc'
+            let rustfilesecpath = __dirname + '/test-tool/tool/' + rust_desc + '.sec'
+            let desc_buffer = decoder(rustfilepath)
+            let [target, buffer] = new cyfs.PeopleDecoder().raw_decode(desc_buffer).unwrap();
 
-    //         //获取属性
-    //         let owner_deco: any = target.desc().owner()?.unwrap().to_base_58();
-    //         let PeopleId_deco: string = target.desc().calculate_id().to_base_58()
-    //         let [ood_list_deco] = target.body_expect().content().ood_list.values()
+            //获取属性
+            let owner_deco: any = target.desc().owner()?.unwrap().to_base_58();
+            let PeopleId_deco: string = target.desc().calculate_id().to_base_58()
+            let [ood_list_deco] = target.body_expect().content().ood_list.values()
 
-    //         //rust解码对象，校验属性值
-    //         let args2 = ' show -a ' + rustfilepath;
-    //         let runInfo2 = await run(ffsClientPath, args2, false, (data: string) => {
-    //             let arr1 = data.split(/: |\n/);
-    //             for (let i in arr1) {
-    //                 let value = arr1[i];
-    //                 let index: any = Number(i) + 1;
-    //                 //校验PeopleId属性
-    //                 if (value == 'objectid') {
-    //                     let objectid_rust = arr1[index]
-    //                     assert.equal(PeopleId_deco, objectid_rust)
-    //                 };
-    //                 //校验owner属性
-    //                 if (value == 'owner') {
-    //                     let owner_rust = arr1[index]
-    //                     assert.equal(owner_deco, owner_rust)
-    //                 }
-    //                 //校验ood_list属性
-    //                 if (value == 'ood_list') {
-    //                     let ood_list_rust = arr1[index]
-    //                     let ood_rust = ood_list_rust.slice(1, ood_list_rust.length - 3)
-    //                     assert.equal(ood_list_deco.to_base_58(), ood_rust)
+            //rust解码对象，校验属性值
+            let args2 = ' show -a ' + rustfilepath;
+            let runInfo2 = await run(ffsClientPath, args2, false, (data: string) => {
+                let arr1 = data.split(/: |\n/);
+                for (let i in arr1) {
+                    let value = arr1[i];
+                    let index: any = Number(i) + 1;
+                    //校验PeopleId属性
+                    if (value == 'objectid') {
+                        let objectid_rust = arr1[index]
+                        assert.equal(PeopleId_deco, objectid_rust)
+                    };
+                    //校验owner属性
+                    if (value == 'owner') {
+                        let owner_rust = arr1[index]
+                        assert.equal(owner_deco, owner_rust)
+                    }
+                    //校验ood_list属性
+                    if (value == 'ood_list') {
+                        let ood_list_rust = arr1[index]
+                        let ood_rust = ood_list_rust.slice(1, ood_list_rust.length - 3)
+                        assert.equal(ood_list_deco.to_base_58(), ood_rust)
 
-    //                     //验证所有属性测试通过，执行删除测试desc文件命令
-    //                     DeleteDescFile(rustfilepath);
-    //                     DeleteDescFile(rustfilesecpath);
-    //                 }
-    //             }
-    //         })
-    //     });
-    // });
+                        //验证所有属性测试通过，执行删除测试desc文件命令
+                        DeleteDescFile(rustfilepath);
+                        DeleteDescFile(rustfilesecpath);
+                    }
+                }
+            })
+        });
+        it("Ts从Rust解码：有效传入owner,ood_list,public,area,name,icon参数", async function () {
+
+            let rust_buffer = decoder(filepath)
+
+            let [target, buffer] = new cyfs.PeopleDecoder().raw_decode(rust_buffer).unwrap();
+
+            //获取属性
+            let owner_deco = target.desc().owner()?.unwrap();
+            let PeopleId_deco = target.desc().calculate_id();
+            let icon_deco = target.icon()
+            let name_deco = target.name();
+            let ood_list_deco = target.body_expect().content().ood_list
+
+            //属性校验
+            assert(owner_deco?.equals(owner), 'owner属性不相等');
+            assert(PeopleId_deco.equals(PeopleId), 'PeopleId属性不相等');
+            assert(icon_deco?.equals(icon), 'icon属性不相等');
+            assert.equal(name, name_deco);
+            for (let i in ood_list_deco) {
+                assert(ood_list_deco[i].equals(ood_list1[i]), 'ood_list属性不相等')
+            }
+
+
+        });
+    });
 })
