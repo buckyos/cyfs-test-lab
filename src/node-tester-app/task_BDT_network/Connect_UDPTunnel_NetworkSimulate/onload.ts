afbf6a3c91d7ef949579aaa9b6fc89d78ef97fc8
lizhihong <lizhihong@buckyos.com>
2022-09-19 11:44:55 +0800

    Add BDT Network Simulate testcase

---
+++ src/node-tester-app/task_BDT_network/Connect_UDPTunnel_NetworkSimulate/onload.ts
@@ -0,0 +1,78 @@
+import {ErrorCode, NetEntry, Namespace, AccessNetType, BufferReader, Logger, TaskClientInterface, ClientExitCode, BufferWriter, RandomGenerator} from '../../base';
+import {TestRunner} from '../../taskTools/cyfs_bdt/testRunner';
+import {Testcase,Task,ActionType,Resp_ep_type} from "../../taskTools/cyfs_bdt/type"
+import {labAgent,BdtPeerClientConfig,LabSnList,randShuffle} from "../../taskTools/cyfs_bdt/labAgent"
+import  * as BDTAction from "../../taskTools/cyfs_bdt/bdtAction"
+import {AgentManager} from '../../taskTools/cyfs_bdt/agentManager'
+
+export async function TaskMain(_interface: TaskClientInterface) {
+    //(1) 连接测试节点
+    let agentManager = AgentManager.createInstance(_interface);
+    await agentManager.initAgentList(labAgent);
+    //(2) 创建测试用例执行器 TestRunner
+    let testRunner = new TestRunner(_interface);
+    let testcaseName = "Connect_UDPTunnel_NetworkSimulate"
+    let testcase:Testcase = {
+        TestcaseName: testcaseName,
+        testcaseId: `${testcaseName}_${Date.now()}`,
+        remark: `LN 和 RN 建立100个BDT连接`,
+        environment: "lab",
+    };
+    await testRunner.initTestcase(testcase);
+    //(3) 创建BDT测试客户端
+    let config : BdtPeerClientConfig = {
+            eps:{
+                ipv4:{
+                    udp:true,
+                }
+            },
+            logType:"info",
+            SN :LabSnList,
+            resp_ep_type:Resp_ep_type.SN_Resp, 
+    }
+    // 每台机器运行一个bdt 客户端
+    await agentManager.allAgentStartBdtPeer(config)
+    //(4) 测试用例执行器添加测试任务
+    for(let [i,j] of randShuffle(labAgent.length)){
+        if(i != j && labAgent[i].tags[0] == "PC_0018" && labAgent[j].tags[0] == "PC_0005" ){
+            let info = await testRunner.createPrevTask({
+                LN : `${labAgent[i].tags[0]}$1`,
+                RN : `${labAgent[j].tags[0]}$1`,
+                timeout : 20*60*1000,
+                action : []
+            })
+            // 1.1 LN 连接 RN
+            let connect_1 =  `${Date.now()}_${RandomGenerator.string(10)}`;
+            info = await testRunner.prevTaskAddAction(new BDTAction.ConnectAction({
+                type : ActionType.connect,
+                LN : `${labAgent[i].tags[0]}$1`,
+                RN : `${labAgent[j].tags[0]}$1`,
+                config:{
+                    conn_tag: connect_1,
+                    timeout : 30*1000,
+                },
+                expect : {err:0},    
+            }))
+            // 1.2 LN 连接 RN 100 次
+            for(let x =0;x<100;x++){
+                let connect_1 =  `${Date.now()}_${RandomGenerator.string(10)}`;
+                info = await testRunner.prevTaskAddAction(new BDTAction.ConnectAction({
+                    type : ActionType.connect,
+                    LN : `${labAgent[i].tags[0]}$1`,
+                    RN : `${labAgent[j].tags[0]}$1`,
+                    config:{
+                        conn_tag: connect_1,
+                        timeout : 30*1000,
+                    },
+                    expect : {err:0},      
+                }))
+            }
+            await testRunner.prevTaskRun();
+        }
+    }
+
+
+    await testRunner.waitFinished()
+    
+    
+}
---
+++ src/node-tester-app/task_BDT_network/Stream_TCPTunnel_NetworkSimulate/onload.ts
@@ -0,0 +1,80 @@
+import {ErrorCode, NetEntry, Namespace, AccessNetType, BufferReader, Logger, TaskClientInterface, ClientExitCode, BufferWriter, RandomGenerator} from '../../base';
+import {TestRunner} from '../../taskTools/cyfs_bdt/testRunner';
+import {Testcase,Task,ActionType,Resp_ep_type} from "../../taskTools/cyfs_bdt/type"
+import {labAgent,BdtPeerClientConfig,LabSnList,randShuffle} from "../../taskTools/cyfs_bdt/labAgent"
+import  * as BDTAction from "../../taskTools/cyfs_bdt/bdtAction"
+import {AgentManager} from '../../taskTools/cyfs_bdt/agentManager'
+
+export async function TaskMain(_interface: TaskClientInterface) {
+    //(1) 连接测试节点
+    let agentManager = AgentManager.createInstance(_interface);
+    await agentManager.initAgentList(labAgent);
+    //(2) 创建测试用例执行器 TestRunner
+    let testRunner = new TestRunner(_interface);
+    let testcaseName = "Stream_TCPTunnel_NetworkSimulate"
+    let testcase:Testcase = {
+        TestcaseName: testcaseName,
+        testcaseId: `${testcaseName}_${Date.now()}`,
+        remark: `LN 和 RN 建立1个BDT连接，持续发送100*10Mb 大小的字节流数据`,
+        environment: "lab",
+    };
+    await testRunner.initTestcase(testcase);
+    //(3) 创建BDT测试客户端
+    let config : BdtPeerClientConfig = {
+            eps:{
+                ipv4:{
+                    udp:true,
+                    tcp:true,
+                }
+            },
+            logType:"info",
+            udp_sn_only : 1,
+            SN :LabSnList,
+            resp_ep_type:Resp_ep_type.SN_Resp, 
+    }
+    // 每台机器运行一个bdt 客户端
+    await agentManager.allAgentStartBdtPeer(config)
+    //(4) 测试用例执行器添加测试任务
+    for(let [i,j] of randShuffle(labAgent.length)){
+        if(i != j && labAgent[i].tags[0] == "PC_0018" && labAgent[j].tags[0] == "PC_0005" ){
+            let info = await testRunner.createPrevTask({
+                LN : `${labAgent[i].tags[0]}$1`,
+                RN : `${labAgent[j].tags[0]}$1`,
+                timeout : 20*60*1000,
+                action : []
+            })
+            // 1.1 LN 连接 RN
+            let connect_1 =  `${Date.now()}_${RandomGenerator.string(10)}`;
+            info = await testRunner.prevTaskAddAction(new BDTAction.ConnectAction({
+                type : ActionType.connect,
+                LN : `${labAgent[i].tags[0]}$1`,
+                RN : `${labAgent[j].tags[0]}$1`,
+                config:{
+                    conn_tag: connect_1,
+                    timeout : 30*1000,
+                },
+                expect : {err:0},    
+            }))
+            // 1.2 LN -> RN 发送数据
+            for(let x =0;x<100;x++){
+                info = await testRunner.prevTaskAddAction(new BDTAction.SendStreamAction({
+                    type : ActionType.send_stream,
+                    LN : `${labAgent[i].tags[0]}$1`,
+                    RN : `${labAgent[j].tags[0]}$1`,
+                    fileSize : 10*1024*1024,
+                    config:{
+                        conn_tag: connect_1,
+                        timeout : 200*1000,
+                    },
+                    expect : {err:0},      
+                }))
+            }
+            await testRunner.prevTaskRun();
+        }
+    }
+
+
+    await testRunner.waitFinished()
+    
+    
+}
