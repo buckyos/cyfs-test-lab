c0c46d871c74f8247667ddd495b0e9b56c1d4f23
yuri-yang <geek8817@gmail.com>
2022-10-31 14:12:40 +0800

    add  RequestGlobalStatePath req_path test

--- src/cyfs-stack-test-typescript/TestSuite/unittest_object_map/test_rmeta_root-state.ts
+++ src/cyfs-stack-test-typescript/TestSuite/unittest_object_map/test_rmeta_root-state.ts
@@ -3622,6 +3622,114 @@ describe("#op-env 初始化方式", function () {
         it("friends路径 跨zone不同dec,access,make()权限组设定", async () => {})
         it("friends路径 跨zone不同dec,access,access,constructor()权限组初始化", async () => {})
          })
+        describe("## SharedCyfsStack.RequestGlobalStatePath，req_path", async () => {
+            it.only("req_path GlobalRoot|Decroot", async () => {
+                let stack_a = (await connect_stack())[1].fork_with_new_dec(cyfs.get_system_dec_app().object_id)
+                console.info(`stack_a,${stack_a.dec_id}`)
+
+                let stack_b = (await connect_stack())[4]
+                console.info(`stack_b,${stack_b.dec_id}`)
+
+            let obj_id1:cyfs.ObjectId  =  (await gen_obj()).desc().object_id();       
+            let obj_id2:cyfs.ObjectId  =  (await gen_obj()).desc().object_id();        
+            let key1 = RandomGenerator.string(10);                
+            let path1 = `/qatest/${key1}/${obj_id1}`
+            const owner_id = stack_a.local_device().desc().owner()!.unwrap();
+            const obj = cyfs.TextObject.create(cyfs.Some(owner_id), 'question', "test_header", "hello!");
+            const object_id: cyfs.ObjectId = obj.desc().calculate_id();
+            const object_raw = obj.to_vec().unwrap();
+
+            let stub_a = stack_a.root_state_stub()
+
+            let stub_b = stack_b.root_state_stub()
+            
+            await stack_a.root_state_meta_stub().clear_access()
+
+
+            await stack_a.root_state_meta_stub().add_access(cyfs.GlobalStatePathAccessItem.new(path1,cyfs.AccessString.full()))
+
+            let path_acc : cyfs.RootStateOpEnvAccess = {
+                path: path1,
+                access: cyfs.AccessPermissions.Full
+            }
+
+            let op_env1 =  (await stub_a.create_path_op_env_with_access(path_acc)).unwrap();
+
+
+            console.info("remove_with_path", await op_env1.remove_with_key(path1,key1))
+
+            let result1 = await op_env1.insert_with_key(path1,key1,obj_id1)
+            console.info(`result1${JSON.stringify(result1)}}`)
+            assert.ok(!result1.err)
+
+            let update_result1 = await op_env1.update()
+            console.info("update_result1",JSON.stringify(update_result1))
+            assert.ok(!update_result1.err)
+
+            await op_env1.remove_with_key(path1,key1)
+
+            await op_env1.update()
+
+            let result2 = await op_env1.insert_with_key(path1,key1,obj_id2)
+            console.info(`result2${JSON.stringify(result2)}}`)
+            assert.ok(!result2.err)
+
+            let update_result2 = await op_env1.update()
+            console.info("update_result2",JSON.stringify(update_result2))
+            assert.ok(!update_result2.err)
+
+
+            await op_env1.commit() 
+
+
+            
+
+            let op_env2 =  (await stub_b.create_path_op_env()).unwrap()
+
+            assert.ok(!(await op_env2.get_by_key(path1,key1)).err)
+
+
+            await stack_a.root_state_meta_stub().clear_access()
+            await stack_b.root_state_meta_stub().clear_access()
+            let acc = cyfs.AccessString.make( [{group:cyfs.AccessGroup.CurrentZone,permissions:cyfs.AccessPermissions.Full}])
+            await stack_b.root_state_meta_stub(undefined,stack_b.dec_id).add_access(cyfs.GlobalStatePathAccessItem.new(path1,cyfs.AccessString.full()))
+
+            let result1_root = update_result1.unwrap().root
+
+            let result2_root = update_result2.unwrap().root
+
+            let path_handler = new cyfs.RequestGlobalStatePath(stack_b.dec_id, path1,cyfs.GlobalStateCategory.RootState,cyfs.RequestGlobalStateRoot.GlobalRoot(result1_root))
+            console.info(`path_handler${JSON.stringify(path_handler)}}`)
+
+            // 添加一个Post Handler
+            {
+                //const handler = new PostObjectHandler();
+                const ret = await stack_b.router_handlers().add_post_object_handler(
+                    cyfs.RouterHandlerChain.Handler,
+                    'post-object-handler',
+                    0,
+                    undefined,
+                    path_handler.toString(),
+                    cyfs.RouterHandlerAction.Default,
+                    new PostObjectHandlerTest());
+        
+                console.info("ret2",ret);
+            }
+        
+            // 测试stack1 Post给 stack2
+            const resp = await stack_a.non_service().post_object({
+                common: {
+                    req_path: path_handler.toString(),
+                    dec_id: stack_a.dec_id,
+                    level: cyfs.NONAPILevel.Router,
+                    flags: 0,
+                    target: stack_b.local_device_id().object_id
+                },
+                object: NONObjectInfo.new_from_object_raw(object_raw).unwrap()
+            });
+            console.info("post_object_resp", resp)
+            })
+        })  
     })
     
 
