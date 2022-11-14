63413ab583826fda62f98e8fafa3bec01f434341
yuri_yang <geek8817@gmail.com>
2022-11-14 17:37:26 +0800

    delete tsc install

--- opt/testcase/cyfs_stack/build_stack.js
+++ opt/testcase/cyfs_stack/build_stack.js
@@ -84,8 +84,6 @@ async function change_sdk(){
 }
 
 async function pack(){
-    run_cmd('npm i  typescript' ,"../../../src/cyfs-stack-test-typescript","cmd")
-    await sleep(10000)
     run_cmd('tsc -p' ,"../../../src/cyfs-stack-test-typescript/tsconfig.json","cmd")
     await sleep(10000)
     //copy package.json to deploy
