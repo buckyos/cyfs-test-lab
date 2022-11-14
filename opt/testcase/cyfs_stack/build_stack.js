e5e2d0bd340cc2e097f09549c570a257f574b554
yuri_yang <geek8817@gmail.com>
2022-11-14 16:54:01 +0800

    add tsc install

--- opt/testcase/cyfs_stack/build_stack.js
+++ opt/testcase/cyfs_stack/build_stack.js
@@ -72,8 +72,8 @@ function run_cmd(cmd,file_path,type){
 
 async function change_sdk(){
     const sdk = process.argv[3];
-    if (sdk == "source") {
-        run_cmd("npm run init source ","../../../src/cyfs-stack-test-typescript/","local")
+    if (sdk == "cyfs-node") {
+        run_cmd("npm run init cyfs-node ","../../../src/cyfs-stack-test-typescript/","local")
 
     }else if (sdk == "cyfs-sdk-nightly") {
         run_cmd("npm run init cyfs-sdk-nightly","../../../src/cyfs-stack-test-typescript/","local")
@@ -84,6 +84,8 @@ async function change_sdk(){
 }
 
 async function pack(){
+    run_cmd('npm i  typescript' ,"../../../src/cyfs-stack-test-typescript/tsconfig.json","cmd")
+    await sleep(10000)
     run_cmd('tsc -p' ,"../../../src/cyfs-stack-test-typescript/tsconfig.json","cmd")
     await sleep(10000)
     //copy package.json to deploy
@@ -92,7 +94,6 @@ async function pack(){
     ,'package.json')
     await sleep(10000)
     run_cmd("npm i","../../../deploy/cyfs-stack-test-typescript","local")
-    args
 }
 
 const type = process.argv[2];
